946eac2814547ba5f925baff1a13d1e7c0ea99b6
jeeban <jeebanjyoti193@gmail.com>
2023-07-18 15:28:46 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace Assignment9
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Name;
+                string Email;
+                string Password;
+                Console.WriteLine("Enter Your Name: ");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter Your Email: ");
+                Email = Console.ReadLine();
+                Console.WriteLine("Enter Your Password: ");
+                Password = Console.ReadLine();
+
+                if (Name.Length < 6)
+                {
+                    throw new ValidationException("Name must have at least 6 characters");
+                }
+                else if (Password.Length < 8)
+                {
+                    throw new ValidationException("Password must have at least 8 character ");
+                }
+                else
+                {
+                    Console.WriteLine("User Registration Success");
+                    Console.WriteLine($"Name: {Name} \nEmail: {Email} \nPassword: {Password}");
+                }
+            }
+            catch (ValidationException ve)
+            {
+                Console.WriteLine("Validation Exception!! " + ve.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!! " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("***  End of Registration  ***");
+                Console.ReadKey();
+            }
+        }
+    }
+}
